b3b8006ef53163e159354cea3c45851645c1e854
luxwarp <mikael.m.carlsson@gmail.com>
2019-07-04 22:04:08 +0200

    removed service worker because app needs internet connection to work. However it's still possible to add to homescreen thnx to manifest file.

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import errorHandler from '@/helpers/errorHandler'
 import 'material-design-icons/iconfont/material-icons.css'
 import 'normalize.css'
 import '@/styles/_layout.scss'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,5 @@ module.exports = {
         data: `@import "~@/styles/_theme.scss";`
       }
     }
-  },
-  pwa: {
-    name: 'Todoify',
-    themeColor: '#0E2B44',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    // configure the workbox plugin
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-    }
   }
 }
