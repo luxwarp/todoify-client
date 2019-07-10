1d749b718508191fc49374a06f898ac7e29b76f5
luxwarp <mikael.m.carlsson@gmail.com>
2019-07-10 22:51:35 +0200

    added some more info on about. More to come soon!

--- src/views/pages/AboutPage.vue
+++ src/views/pages/AboutPage.vue
@@ -1,8 +1,43 @@
 <template>
   <div class="container">
+    <div class="hero">
+      <div class="title">Todoify</div>
+      <div class="subTitle">Organize and take control over your life</div>
+    </div>
+    <div class="container text-center">
+      <p>
+        Todoify is a simple but great open source to-do app. With Todoify you
+        can organize your tasks in categories.
+      </p>
+    </div>
     <div class="container">
-      <h2>About</h2>
-      <p>Text</p>
+      <div class="row no-wrap full-width">
+        <div class="iconCard">
+          <i class="material-icons icon">devices</i>
+          <div class="title">
+            Use on any device
+          </div>
+        </div>
+        <div class="iconCard">
+          <i class="material-icons icon">money_off</i>
+          <div class="title">
+            Completely free
+          </div>
+        </div>
+        <div class="iconCard">
+          <i class="material-icons icon">code</i>
+          <div class="title">
+            Open Source
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container text-center">
+      <p>
+        If you create an account your to-do's and categories will be synced to
+        our server so you can access it on any device with internet connection.
+        <strong>That's amazing!</strong>
+      </p>
     </div>
   </div>
 </template>
