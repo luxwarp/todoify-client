18d24753709290018b266a29284ecee5e0ea9a59
luxwarp <mikael.m.carlsson@gmail.com>
2019-07-19 19:25:42 +0200

    added base for syncCategories action that will replace getCategories action.

--- src/store/modules/categories.js
+++ src/store/modules/categories.js
@@ -35,6 +35,39 @@ const actions = {
       console.log(error);
     }
   },
+  async syncCategories({ commit, state }) {
+    try {
+      const localCategories = state.categories;
+      const response = await window.$todoify.getCategories();
+      const remoteCategories = response.data.data;
+
+      let syncedCategories = [];
+
+      // If no local categories all remote categories adds to state.
+      if (!localCategories.length) {
+        console.log("No local categories. Setting all remote.");
+        commit("setCategories", remoteCategories);
+        return;
+      }
+
+      // If no remote categories. Sending all local categories to server.
+      if (!remoteCategories.length) {
+        console.log(
+          "No remote categories. Sending all local categories to server."
+        );
+        localCategories.forEach(async localCategory => {
+          const response = await window.$todoify.createCategory(localCategory);
+          syncedCategories.push(response.data.data);
+        });
+        commit("setCategories", syncedCategories);
+        return;
+      }
+    } catch (error) {
+      commit("createNotifier", { type: "error", message: error.message });
+      console.log(error);
+      return;
+    }
+  },
   async createCategory({ commit, getters }, title) {
     try {
       const newCategory = {
