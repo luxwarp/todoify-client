dd2f4560d64cb5df03a63e0fd7b0756e8d037171
luxwarp <mikael.m.carlsson@gmail.com>
2019-07-30 16:29:57 +0200

    converted bottomNav to a ul list.

--- src/components/layout/BottomNav/BottomNav.vue
+++ src/components/layout/BottomNav/BottomNav.vue
@@ -1,40 +1,31 @@
 <template>
-  <div class="bottomNav">
-    <router-link :to="{ name: 'todos.list' }" title="To-do's" class="navItem">
-      <i class="icon-list"></i>
-    </router-link>
-    <router-link
-      :to="{ name: 'categories.list' }"
-      title="Categories"
-      class="navItem"
-    >
-      <i class="icon-folder"></i>
-    </router-link>
-    <router-link
-      v-if="isOnline() && isAuth()"
-      :to="{ name: 'user.profile' }"
-      title="Profile"
-      class="navItem"
-    >
-      <i class="icon-user"></i>
-    </router-link>
-    <router-link
-      v-else-if="isOnline() && !isAuth()"
-      :to="{ name: 'user.login' }"
-      title="Login"
-      class="navItem"
-    >
-      <i class="icon-login"></i>
-    </router-link>
-    <router-link
-      v-else
-      :to="{ name: 'home.page' }"
-      title="Home"
-      class="navItem"
-    >
-      <i class="icon-home"></i>
-    </router-link>
-  </div>
+  <ul class="bottomNav">
+    <li class="navItem">
+      <router-link :to="{ name: 'todos.list' }" title="To-do's">
+        <i class="icon-list"></i>
+      </router-link>
+    </li>
+    <li class="navItem">
+      <router-link :to="{ name: 'categories.list' }" title="Categories">
+        <i class="icon-folder"></i>
+      </router-link>
+    </li>
+    <li v-if="isOnline() && isAuth()" class="navItem">
+      <router-link :to="{ name: 'user.profile' }" title="Profile">
+        <i class="icon-user"></i>
+      </router-link>
+    </li>
+    <li v-else-if="isOnline() && !isAuth()" class="navItem">
+      <router-link :to="{ name: 'user.login' }" title="Login">
+        <i class="icon-login"></i>
+      </router-link>
+    </li>
+    <li v-else class="navItem">
+      <router-link :to="{ name: 'home.page' }" title="Home">
+        <i class="icon-home"></i>
+      </router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -56,17 +47,23 @@ export default {
 
   > .navItem {
     flex: 1;
-    padding: 10px 0;
     color: inherit;
     text-align: center;
     font-size: 1.2rem;
     transition: all 0.4s ease-in-out;
+    display: flex;
 
     &:hover,
     &:active,
     &:focus,
     &.router-link-exact-active {
       background: $bottomNavHoverBgColor;
+    }
+
+    > a {
+      flex: 1;
+      color: inherit;
+      padding: 10px 0;
     }
   }
 }
