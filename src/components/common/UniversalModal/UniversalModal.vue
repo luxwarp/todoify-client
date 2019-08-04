cdc9476140781d620f849aaa58a74a54a91a9cba
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-04 18:29:23 +0200

    fixed overflow on modal and changed close button to a button.

--- src/components/common/UniversalModal/UniversalModal.vue
+++ src/components/common/UniversalModal/UniversalModal.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="universalModal">
     <div class="container">
-      <div class="title">
+      <h1 class="title">
         <slot name="title">title slot</slot>
-        <i class="icon-cancel close" @click="close"></i>
-      </div>
+        <button class="close" @click="close">
+          <i class="icon-cancel"></i>
+        </button>
+      </h1>
       <div class="body">
         <slot name="body">body slot</slot>
       </div>
@@ -23,42 +25,52 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .universalModal {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background: rgba(0, 0, 0, 0.3);
-  display: flex;
   padding: 15px;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   z-index: 99999;
+  overflow: auto;
+  display: flex;
 
   > .container {
-    display: flex;
-    flex-direction: column;
     width: 100%;
     max-width: 600px;
     background: #fff;
-    padding: 0 15px;
+    padding: 15px;
+    margin: auto;
+    border-radius: 5px;
 
     > .title {
       display: flex;
       flex-direction: row;
       justify-content: space-between;
+      margin: 0 0 15px 0;
+      padding: 0;
+      font-size: 1.2rem;
+      font-weight: 300;
 
       > .close {
         cursor: pointer;
+        appearance: none;
+        background: none;
+        border: 0;
+        padding: 0;
       }
     }
 
     > .body {
       display: flex;
       flex-direction: column;
+
+      > :last-child {
+        margin-bottom: 0;
+      }
     }
   }
 }
