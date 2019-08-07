2c64ab947f037e24a7338645dbb6dfd31baf675d
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-07 17:25:17 +0200

    finishing touches to updatebanner.

--- src/components/ui/NewUpdateBanner/NewUpdateBanner.vue
+++ src/components/ui/NewUpdateBanner/NewUpdateBanner.vue
@@ -1,12 +1,12 @@
 <template>
   <transition name="slideInBottom" appear>
-    <div v-if="show" class="newUpdateBanner">
-      <button class="close" @click="$emit('close')">
+    <div class="newUpdateBanner">
+      <button class="button noStyle close" @click="$emit('close')">
         <i class="icon-cancel"></i>
       </button>
       <div>
         <p>
-          New version available. <br />
+          A new version is available. <br />
           <a
             href="https://github.com/luxwarp/todoify-client/wiki"
             target="_blank"
@@ -16,8 +16,9 @@
           </a>
         </p>
       </div>
-      <button class="button" @click="$emit('confirm')">
-        Update
+      <button class="button success" @click="$emit('confirm')">
+        <span class="label">Update</span>
+        <i class="icon-ok" />
       </button>
     </div>
   </transition>
@@ -25,12 +26,7 @@
 
 <script>
 export default {
-  name: "NewUpdateBanner",
-  data() {
-    return {
-      show: true
-    };
-  }
+  name: "NewUpdateBanner"
 };
 </script>
 
@@ -46,37 +42,22 @@ export default {
   display: grid;
   grid-template-columns: auto 1fr auto;
   align-items: center;
-
   color: #000;
   z-index: 99999999;
 
   > .close {
     margin-right: 15px;
-    font-weight: bold;
     color: red;
     cursor: pointer;
-    background: none;
-    padding: 0;
-  }
-
-  > button {
-    padding: 5px;
-    background: green;
-    color: #fff;
-    border-radius: 5px;
-    margin-left: 10px;
-    cursor: pointer;
-    border: 0;
-    margin-bottom: 0;
   }
 }
 
 .slideInBottom-enter-active {
-  animation: slideInBottom 0.3s;
+  animation: slideInBottom 0.3s forwards;
 }
 
 .slideInBottom-leave-active {
-  animation: slideInBottom 0.3s reverse;
+  animation: slideInBottom 0.3s reverse forwards;
 }
 
 @keyframes slideInBottom {
