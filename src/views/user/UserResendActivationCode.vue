db7b3f5be24898c07d36d725a51d01e9027dbfb6
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-10 20:14:34 +0200

    userresendactivationcode restructure to l-card

--- src/views/user/UserResendActivationCode.vue
+++ src/views/user/UserResendActivationCode.vue
@@ -1,35 +1,34 @@
 <template>
-  <div class="container center">
+  <div class="userresendactivationcode">
     <BrandHero />
-    <div class="card noFullWidth hcenter">
-      <div class="title">
+    <l-card>
+      <template v-slot:header>
         <span class="label">Resend Activation Code</span>
-      </div>
-      <div class="body">
-        <p>
-          Enter your email adress to request a new activation code for your
-          account.
-        </p>
-        <form autocomplete="off" @submit.prevent="onSubmit">
-          <input
-            v-model="email"
-            v-focus
-            type="email"
-            placeholder="Email"
-            name="email"
-            required
-            autocomplete="off"
-          />
-          <div class="row">
-            <button class="button primary" type="submit">
-              <span class="label">Request code</span>
-              <i class="icon-key"></i>
-            </button>
-            <router-link :to="{ name: 'user.activate' }">Back</router-link>
-          </div>
-        </form>
-      </div>
-    </div>
+      </template>
+
+      <p>
+        Enter your email adress to request a new activation code for your
+        account.
+      </p>
+      <form autocomplete="off" @submit.prevent="onSubmit">
+        <input
+          v-model="email"
+          v-focus
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+          autocomplete="off"
+        />
+        <div class="buttons">
+          <button class="button primary" type="submit">
+            <span class="label">Request code</span>
+            <i class="icon-key"></i>
+          </button>
+          <router-link :to="{ name: 'user.activate' }">Back</router-link>
+        </div>
+      </form>
+    </l-card>
   </div>
 </template>
 
@@ -54,12 +53,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.row {
-  justify-content: space-between;
-  align-items: center;
+.userresendactivationcode {
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
 
-  > * {
-    margin-bottom: 0;
+  .buttons {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
