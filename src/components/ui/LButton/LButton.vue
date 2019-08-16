8b2b49bf3e8edc3689c82c9a3a129509a54f9869
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-16 11:55:06 +0200

    added noStyle class for LButton.

--- src/components/ui/LButton/LButton.vue
+++ src/components/ui/LButton/LButton.vue
@@ -1,16 +1,33 @@
 <template>
-  <button class="lButton">
-    <slot>lButton</slot>
+  <button
+    class="lButton"
+    :class="`lButton__${design}`"
+    @click="$emit('click', $event)"
+  >
+    <slot>LButton slot</slot>
   </button>
 </template>
 
 <script>
 export default {
-  name: "LButton"
+  name: "LButton",
+  props: {
+    design: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .lButton {
+  cursor: pointer;
+  &.lButton__noStyle {
+    background: none;
+    border: 0;
+    color: inherit;
+  }
 }
 </style>
