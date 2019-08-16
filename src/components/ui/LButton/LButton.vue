09dbe4427f6d0cd106282d41184348aac57c6c43
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-16 11:30:14 +0200

    added base for global component LButton.

---
+++ src/components/ui/LButton/LButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="lButton">
+    <slot>lButton</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "LButton"
+};
+</script>
+
+<style lang="scss" scoped>
+.lButton {
+}
+</style>
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,8 +1,17 @@
 import BrandHero from "./BrandHero";
+import LButton from "./LButton";
 import LCard from "./LCard";
 import LList from "./LList";
 import NoListItemsFound from "./NoListItemsFound";
 import ToolBox from "./ToolBox";
 import UniversalModal from "./UniversalModal";
 
-export { BrandHero, LCard, LList, NoListItemsFound, ToolBox, UniversalModal };
+export {
+  BrandHero,
+  LButton,
+  LCard,
+  LList,
+  NoListItemsFound,
+  ToolBox,
+  UniversalModal
+};
