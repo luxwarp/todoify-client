9f9aae8149647d7f74a51a7596a5890f7498499b
luxwarp <mikael.m.carlsson@gmail.com>
2019-08-16 12:21:58 +0200

    added success, alert, and primary designs to LButton.

--- src/components/ui/LButton/LButton.vue
+++ src/components/ui/LButton/LButton.vue
@@ -25,11 +25,54 @@ export default {
 <style lang="scss" scoped>
 .lButton {
   cursor: pointer;
+  transition: all 0.4s;
+  width: max-content;
+  display: block;
+
+  &:disabled {
+    background: $buttonBgColor !important;
+    color: $buttonFontColor;
+    cursor: not-allowed !important;
+  }
+
   &.lButton__noStyle {
     background: none;
-    border: 0;
     color: inherit;
     padding: 0;
+    border: 0;
+  }
+
+  &.lButton__primary {
+    background: #30465c;
+    color: #fff;
+
+    &:hover,
+    &:active,
+    &:focus {
+      background: lighten($color: #30465c, $amount: 10%);
+    }
+  }
+
+  &.lButton__success {
+    background: #5aaf50;
+    color: #fff;
+
+    &:hover,
+    &:active,
+    &:focus {
+      background: lighten($color: #5aaf50, $amount: 10%);
+    }
+  }
+
+  &.lButton__alert {
+    background: #ef4835;
+    color: #fff;
+
+    &:hover,
+    &:active,
+    &:focus {
+      background: lighten($color: #ef4835, $amount: 10%);
+    }
   }
 }
 </style>
